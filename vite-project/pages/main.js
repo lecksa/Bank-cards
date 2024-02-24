83bc40aff197c2e266aa2523fd1c5d4eaa04a0b4
Dilrukh <146838835+lecksa@users.noreply.github.com>
2024-02-24 14:15:42 +0500

    Update main.js
    Added

---
+++ vite-project/pages/main.js
@@ -0,0 +1,6 @@
+let email = document.querySelector("#email")
+let name = document.querySelector("#name")
+let surname = document.querySelector("#surname")
+let password = document.querySelector("#password")
+let btn = document.querySelector("button")
+
