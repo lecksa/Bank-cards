4c42136f1ab03eb4767d2391589b3b65c9373151
Dilrukh <146838835+lecksa@users.noreply.github.com>
2024-02-24 14:58:38 +0500

    Update main.js
    Added

--- vite-project/pages/main.js
+++ vite-project/pages/main.js
@@ -1,6 +1,23 @@
-let email = document.querySelector("#email")
-let name = document.querySelector("#name")
-let surname = document.querySelector("#surname")
-let password = document.querySelector("#password")
+let email_inp = document.querySelector("#email")
+let name_inp = document.querySelector("#name")
+let surname_inp = document.querySelector("#surname")
+let password_inp = document.querySelector("#password")
 let btn = document.querySelector("button")
 
+let storage = JSON.parse(localStorage.getItem("user", user)
+
+let user = {
+  email: email_inp.value,
+  name: name_inp.value,
+  surname: surname_inp.value,
+  password: password_inp.value
+}
+
+email_inp.value = storage.email
+name_inp.value = storage.name
+surname_inp.value = storage.surname
+password_inp.value = storage.password
+
+btn.onclick = () => {
+  JSON.stringify(localStorage.setItem("user", user))
+}
